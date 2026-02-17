6b03a1d4d4b0d3f255637cd4b36aa1ecdeb0410b
marianoimp <nolitomh@gmail.com>
2026-02-17 02:32:37 -0300

    Continuo con la estructura backend para tomar información de una base de datos

--- back/app.py
+++ back/app.py
@@ -1,2 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
+from routes.productos import productos_bp
 
+app = Flask(__name__)
+CORS(app)
+
+app.register_blueprint(productos_bp, url_prefix="/productos")
+
+if __name__=="__name__":
+    app.run(port=5053,debug=True)
---
+++ back/datos.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE IF NOT EXISTS Yerbas;
+
+USE Yerbas;
+
+CREATE TABLE productos (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	nombre VARCHAR(100),
+    precio INT,
+    stock INT DEFAULT 5
+);
+
+INSERT INTO productos(nombre,precio) VALUES('TERMOS', 5000);
+
+
+CREATE TABLE usuarios (
+    id_usuarios INT PRIMARY KEY AUTOINCREMENT,
+    nombre VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    password VARCHAR(250)
+
+);
+
+INSERT INTO usuarios (nombre,email,password) VALUES ('Juan','jp@gmail.com','1234');
+
+CREATE TABLE carrito (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT,
+    producto_id INT,
+    cantidad INT,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+    FOREIGN KEY (producto_id) REFERENCES productos(id)
+);
